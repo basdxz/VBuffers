5386e01852243730427be6d4c8254281b0a8b822
basdxz <tudurap.com@gmail.com>
2022-12-09 11:35:34 +0000

    Deleted old access front stuff

--- src/main/java/com/github/basdxz/vbuffers/access/front/impl/AccessFrontFactory.java
+++ src/main/java/com/github/basdxz/vbuffers/access/front/impl/AccessFrontFactory.java
@@ -1,29 +1,11 @@
 package com.github.basdxz.vbuffers.access.front.impl;
 
-import com.github.basdxz.vbuffers.access.back.SetterBack;
-import com.github.basdxz.vbuffers.access.back.impl.AccessorBacks;
 import com.github.basdxz.vbuffers.access.front.AccessFront;
-import com.github.basdxz.vbuffers.access.front.SetterFront;
-import lombok.*;
 
 import java.lang.reflect.Method;
-import java.nio.ByteBuffer;
 
 public class AccessFrontFactory {
-    public boolean isSupported(Method method) {
-        if (!method.isAnnotationPresent(SetterFront.Access.class))
-            return false;
-        if (method.getReturnType() != void.class)
-            return false;
-        return method.getParameterTypes().length == 1;
-    }
-
-    @SuppressWarnings("unchecked")
     public AccessFront create(Method method) {
-        val setter = (SetterBack<Object>) AccessorBacks.setter(method.getParameterTypes()[0]);
-        return (SetterFront) args -> {
-            setter.put((ByteBuffer) args[0], (int) args[1], args[2]);
-            return null;
-        };
+        return null;
     }
 }
